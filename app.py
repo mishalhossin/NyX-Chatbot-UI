c149ad3f11785aec4354c4d7461dd7b32c428ce2
mishalhossin <mishalhossin20@gmail.com>
2023-10-23 02:11:15 +0600

    One gpt model

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import openai
 from config import INSTRUCTIONS
 
 models = [
-    "gpt-3.5-turbo-16k-0613",
+    "gpt-3.5-turbo-0613",
     "meta-llama/Llama-2-70b-chat-hf",
     "jondurbin/airoboros-l2-70b-gpt4-1.4.1",
     "mistralai/Mistral-7B-Instruct-v0.1",
